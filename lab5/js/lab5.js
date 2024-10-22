c1b1cbe01e1cd3845fd85fe68f06fa9f1363e65d
Ikennaoh <Ojhiaeri@gmail.com>
2024-10-21 20:16:19 -0700

    labs

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+/**
+ * Author:    Ikenna Ohiaeri <iohiaeri@ucsc.edu>
+ * Created:   10.21.2024
+ * Liceense: Public Domain
+ **/
+
+//Variables
+make = "Lectric";
+model = "Xpedition";
+color = "White";
+year = 2023;
+
+//Age caclculation
+age = 2024-year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model+ "<br>");
+document.writeln("Color " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
